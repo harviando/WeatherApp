e23553d0272efa8f5604b9aa05f413ad08605ccb
Muhammad Harviando <harviandomuhammad@gmail.com>
2021-09-16 14:17:56 +0700

    added documentation

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -18,38 +18,42 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        searchTextField.delegate = self
+        searchTextField.delegate = self // untuk mengassign delegate pada searchTextField
     }
 
+    //Fungsi yang dijalankan ketika button search di tekan
     @IBAction func searchPressed(_ sender: UIButton) {
         
-        changeCityLabel(toName: searchTextField.text!)
+        changeCityLabel(toName: searchTextField.text!) // mengubah label city sesuai dengan teks di searchTextField
         
-        searchTextField.endEditing(true)
+        searchTextField.endEditing(true) //Untuk mendissmiss keyboard melalui delegate
     }
     
+    //Fungsi yang mendeteksi tombol "go" pada keyboard di tekan
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
-        changeCityLabel(toName: searchTextField.text!)
+        changeCityLabel(toName: searchTextField.text!) // mengubah label city sesuai dengan teks di searchTextField
         
-        searchTextField.endEditing(true)
-        return true
+        searchTextField.endEditing(true) //Untuk mendissmiss keyboard melalui delegate
+        return true // me-return nilai bool yang diminta pada fungsi
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         if textField.text != "" { //mendeteksi kalau textfield ada tulisan
-            return true
+            return true // me-return nilai bool yang diminta pada fungsi
         } else {
-            textField.placeholder = "Type Something" //mengunchi keyboard kalau textfield gak ada tulisan
-            return false
+            textField.placeholder = "Type Something" //mengunci keyboard kalau textfield tidak ada tulisan
+            return false // me-return nilai bool yang diminta pada fungsi
         }
     }
     
+    // Mendeteksi kalau user telah selesai memasukkan input pada textfield
     func textFieldDidEndEditing(_ textField: UITextField) {
         
-        searchTextField.text = ""
+        searchTextField.text = "" // mengosongkan teks pada searchTextField.text
     }
     
+    //Fungsi untuk mengubah label cityLabel sesuai dengan Teks pada seacrhFieldText
     func changeCityLabel(toName: String) {
         cityLabel.text = toName
     }
